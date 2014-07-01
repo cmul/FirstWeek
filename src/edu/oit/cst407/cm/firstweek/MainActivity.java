4302628a4b22205b2eb7b891bb9012e6f8748066
cmul <cmul@users.noreply.github.com>
2014-06-30 23:26:01 -0700

    First building and showing app on phone. Also removing /bin and /gen files, updating .ignore file to hopefully ignore those directories.

---
+++ src/edu/oit/cst407/cm/firstweek/MainActivity.java
@@ -0,0 +1,12 @@
+package edu.oit.cst407.cm.firstweek;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		//setContentView(R.layout.activity_main);
+	}
+
+}
