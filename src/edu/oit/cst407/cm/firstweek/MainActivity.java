6c3b1327c3d42072ed55f60af4e3ed2de9b767d2
cmul <cmul@users.noreply.github.com>
2014-07-02 00:06:23 -0700

    working button and text field--they show up

--- src/edu/oit/cst407/cm/firstweek/MainActivity.java
+++ src/edu/oit/cst407/cm/firstweek/MainActivity.java
@@ -4,9 +4,16 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
+<<<<<<< HEAD
+	@Override
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+=======
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		//setContentView(R.layout.activity_main);
+>>>>>>> parent of 6a9c0cb... App where buttons, text field show up. Removing extra FirstWeek
 	}
 
 }
