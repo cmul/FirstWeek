4dc4c3a8e277ab5e3c6394dc6b600197f840fe2b
cmul <cmul@users.noreply.github.com>
2014-07-05 16:28:16 -0700

    Camera Working
    Added code to get a camera picture back and display it. Added pictures
    to show no image has been taken.

--- src/edu/oit/cst407/cm/firstweek/MainActivity.java
+++ src/edu/oit/cst407/cm/firstweek/MainActivity.java
@@ -1,19 +1,43 @@
 package edu.oit.cst407.cm.firstweek;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
-<<<<<<< HEAD
+	static final int REQUEST_IMAGE_CAPTURE = 3000; //
+	private Button buttonTakePicture = null;
+	private ImageView myImageView = null;
+	
 	@Override
-	public void onCreate(Bundle savedInstanceState){
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-=======
-	public void onCreate(Bundle savedInstanceState){
-		super.onCreate(savedInstanceState);
-		//setContentView(R.layout.activity_main);
->>>>>>> parent of 6a9c0cb... App where buttons, text field show up. Removing extra FirstWeek
+	public void onCreate(Bundle savedInstanceState){ //
+		super.onCreate(savedInstanceState);			//	
+		setContentView(R.layout.activity_main);		//
+		
+		myImageView = (ImageView)findViewById(R.id.imagePhoto); //
+		buttonTakePicture = (Button)findViewById (R.id.buttonSelectImage); //
+		
+		buttonTakePicture.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent takePictureIntent = new Intent (MediaStore.ACTION_IMAGE_CAPTURE);
+				if(takePictureIntent.resolveActivity(getPackageManager()) != null ) {
+					startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+				}
+			}
+		});
 	}
-
+	
+	protected void onActivityResult(int requestCode, int resultCode, Intent data){
+		if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
+			Bitmap photo = (Bitmap) data.getExtras().get("data");
+			myImageView.setImageBitmap(photo);
+		}
+	}
 }
